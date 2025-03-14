544f68af1d8eba09b0aaf3ef28ce53198c498f7d
Nguyễn Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2025-03-15 05:03:21 +0700

    Update ProgressPage.js
    warning push code

--- frontend/src/pages/ProgressPage.js
+++ frontend/src/pages/ProgressPage.js
@@ -4,7 +4,7 @@ const ProgressPage = () => {
   return (
     <div className="p-6 md:p-8 max-w-5xl mx-auto">
       <h1 className="text-3xl font-bold text-gray-800 mb-4">Frontend Developer 4</h1>
-      <p className="text-lg text-red-500">Chưa thực hiện push code</p>
+      <p className="text-lg text-red-500">"You haven't pushed your code yet, please check and do it soon!"</p>
     </div>
   );
 };
