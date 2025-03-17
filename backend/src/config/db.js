815e6166fe8d7c0781bfccc60a75c946f5d3fe9f
Nguyễn Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2025-03-17 15:56:40 +0700

    Update db.js
    new

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,8 +7,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 100,
-  queueLimit: 0
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
 });
 
 module.exports = pool.promise(); 
