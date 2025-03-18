ea07f327dea81f247be54153f610851a80d6a38b
Nguyễn Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2025-03-18 22:49:35 +0700

    Update Footer.js
    update verson

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <center>Cập nhật lúc 14/03/2025 - Version 1.2.0</center>
+        <center>Cập nhật lúc 18/03/2025 - Version 1.4.0</center>
       </div>
     </footer>
   );
