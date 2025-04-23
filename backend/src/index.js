54ff8c45d544051f44bb2bc32686c2938a48e46c
Nguyễn Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2025-04-23 23:28:19 +0700

    Update index.js
    update limit resq

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,8 +23,8 @@ app.use(compression());
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 10000, // 15 minutes
+  max: 10000 // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 app.use(cors());
