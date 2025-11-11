c9729435c2c5a8be930f10484f8300d788e14558
ngDuyHung <nguyenduyhung2456@gmail.com>
2025-11-11 11:25:08 +0700

    đ

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -57,6 +57,7 @@ const AuthPage = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-500 to-blue-700 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <h1>Hello</h1>
       <div className="sm:mx-auto sm:w-full sm:max-w-md text-center">
         <h1 className="text-3xl font-bold text-white mb-2">English Learning HDPTAK</h1>
         <h2 className="mt-6 text-center text-2xl font-bold text-white">
